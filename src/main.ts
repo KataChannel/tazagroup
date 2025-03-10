1e92758cd2d43c6a0618050730bcb48beb9908a1
quocbao1983 <quocbao280783@gmail.com>
2025-03-10 23:09:22 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,29 @@ import { AppModule } from './app.module';
 import * as express from 'express';
 import { join } from 'path';
 import * as bodyParser from 'body-parser';
-declare const module: any;
+import * as admin from 'firebase-admin';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.use('/images', express.static(join(__dirname, '../sandbox/images')));
   app.use(bodyParser.json({ limit: '50mb' }));
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-  await app.listen(process.env.PORT ?? 3331);
-  if (module.hot) {
-    module.hot.accept();
-    module.hot.dispose(() => app.close());
+
+  // Lấy Service Account từ biến môi trường
+  if (process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON) {
+    const serviceAccount = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON);
+
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+    });
+
+    console.log('✅ Firebase initialized with Service Account from ENV');
+  } else {
+    console.error('❌ GOOGLE_APPLICATION_CREDENTIALS_JSON is missing!');
   }
+
+  await app.listen(process.env.PORT ?? 3331);
 }
+
 bootstrap();
