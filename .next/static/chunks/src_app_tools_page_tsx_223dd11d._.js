924546b743b7916902f42b8e09d1fc0cdc3595de
Kata <katachanneloffical@gmail.com>
2025-07-22 23:21:43 +0700

    Xóa node_modules và .next khỏi Git

--- .next/static/chunks/src_app_tools_page_tsx_223dd11d._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_89791d07._.js",
-  "static/chunks/src_45eb65b8._.js"
-],
-    source: "dynamic"
-});
--- .next/static/chunks/src_app_training_page_tsx_223dd11d._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_aac8b632._.js",
-  "static/chunks/node_modules_6ff0861d._.js"
-],
-    source: "dynamic"
-});
